15986c39dc36c33b35c604dae330edcf4e8b4786
srushtidevkar893-cell <srushtidevkar893@gmail.com>
2026-09-04 13:36:12 +0530

    Add files via upload

--- practicalno9.py
+++ practicalno9.py
@@ -1,26 +1,63 @@
- # Student Marks Management System
+# Student Marks Management System
 
-marks = [70, 80, 65, 90]
+print("----- Student Marks Management System -----")
 
-# Display marks - Traversal
-print("Original Marks:")
-for mark in marks:
-    print(mark)
+marks = []
 
-# Insert a new mark
-marks.insert(2, 75)
+# Taking marks from user
+n = int(input("Enter number of subjects: "))
 
-print("\nAfter Insertion:")
-print(marks)
+for i in range(n):
+    mark = int(input(f"Enter marks for Subject {i+1}: "))
+    marks.append(mark)
 
-# Update a mark
-marks[1] = 85
+while True:
 
-print("\nAfter Updating:")
-print(marks)
+    print("\n--------- MENU ---------")
+    print("1. Display Marks")
+    print("2. Insert New Mark")
+    print("3. Update Mark")
+    print("4. Delete Mark")
+    print("5. Exit")
 
-# Delete a mark
-marks.remove(65)
+    choice = int(input("Enter your choice: "))
 
-print("\nAfter Deletion:")
-print(marks)
+    if choice == 1:
+        print("\nOriginal Marks:")
+        for mark in marks:
+            print(mark)
+
+    elif choice == 2:
+        position = int(input("Enter position: "))
+        mark = int(input("Enter new mark: "))
+
+        marks.insert(position, mark)
+
+        print("After Insertion:")
+        print(marks)
+
+    elif choice == 3:
+        position = int(input("Enter position to update: "))
+        mark = int(input("Enter new mark: "))
+
+        marks[position] = mark
+
+        print("After Updating:")
+        print(marks)
+
+    elif choice == 4:
+        mark = int(input("Enter mark to delete: "))
+
+        if mark in marks:
+            marks.remove(mark)
+            print("After Deletion:")
+            print(marks)
+        else:
+            print("Mark not found!")
+
+    elif choice == 5:
+        print("Thank you!")
+        break
+
+    else:
+        print("Invalid choice!")
